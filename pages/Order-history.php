5ee823cc8cc1e93da4bc5272a39b35abbeabe428
Phersy <pjncapati727@gmail.com>
2022-01-18 02:58:18 +0800

    cart,Order History,User
    add cart,order history and user page and user profile css.

---
+++ pages/Order-history.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/user-profile.css">
+    <!-- FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poor+Story&family=Roboto:wght@300&family=Satisfy&display=swap" rel="stylesheet">
+    
+    <title>Escafe - Login</title>
+</head>
+<body>
+
+    <!-- HEADER -->
+    <header>
+        <div class="logo-name">
+            <div class="logo-head"><img src="../img/index/logo.png" alt="logo"></div>
+            <div class="name-head"><p>escafé<p></div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Register</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    
+    <main>
+
+        <div class="table-rec">
+        <table>
+            <thead>
+                <tr>
+                <th>Product Name </th>
+                <th>Price </th>
+                <th>Quantity </th>
+                <th>Total Price </th>
+                <th>Status </th>
+                <th>Actions </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+            </tr>
+            </tbody>
+        </table>
+        </div>
+
+    </main>
+
+    <!-- FOOTER -->
+    <footer>
+        <p>&copy; 2022</p>
+    </footer>
+</body>
+</html>
---
+++ pages/user-profile.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/user-profile.css">
+    <!-- FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poor+Story&family=Roboto:wght@300&family=Satisfy&display=swap" rel="stylesheet">
+    
+    <title>Escafe - Login</title>
+</head>
+<body>
+
+    <!-- HEADER -->
+    <header>
+        <div class="logo-name">
+            <div class="logo-head"><img src="../img/index/logo.png" alt="logo"></div>
+            <div class="name-head"><p>escafé<p></div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Register</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    
+    <main>
+        <div class="login-form">
+        <form>
+            <label for="fname">First name:</label><br>
+            <input type="text" id="fname" name="fname"><br>
+
+            <label for="lname">Last name:</label><br>
+            <input type="text" id="lname" name="lname"><br>
+
+            <label for="email">E-mail:</label><br>
+            <input type="text" id="email" name="email"><br>
+
+            <label for="address">Address:</label><br>
+            <input type="text" id="address" name="address"><br>
+
+            <label for="username">Username:</label><br>
+            <input type="text" id="username" name="username"><br>
+
+            <label for="password">Password:</label><br>
+            <input type="text" id="password" name="password"><br>
+        </form>
+        </div>
+    </main>
+
+    <!-- FOOTER -->
+    <footer>
+        <p>&copy; 2022</p>
+    </footer>
+</body>
+</html>
